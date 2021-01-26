a254cf6c665c897d90dd9a56bd0ea7cbf014492e
kai7337 <54688335+kai7337@users.noreply.github.com>
2021-01-25 16:16:38 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+
+// Author Kai Choi
+// Description: Hello World in C++
+//============================================================
+
+#include <iostream>
+  using namespace std;
+  
+  int hello1Function(string name1);
+  int hello2Function(string name2);
+ //main
+  int main(){
+  string name1="Kai Choi";
+  
+  hello1Function(name1);
+  
+  string name2="Choi Kai";
+  
+  hello2Function(name2);
+  
+  return 0;
+  }
+  
