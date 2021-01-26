61ffe9399326d8b40275009025071a3f62b82bbf
kai7337 <54688335+kai7337@users.noreply.github.com>
2021-01-25 16:18:26 -0800

    Create hello1function.cpp

---
+++ hello1function.cpp
@@ -0,0 +1,10 @@
+  /** hello1Function.cpp**/
+  
+  #include <iostream>
+  
+  using namespace std;
+  
+  inthello1Function(string name1){
+    cout << "hello World "<< name1 << endl;
+    return 0;
+  }
